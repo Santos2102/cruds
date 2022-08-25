08d7d99eb7b1b14d7712912a2eab048210b3b4c2
Santos2102 <mikesan2102@umes.edu.gt>
2022-08-24 19:12:08 -0600

    Implementacion de diseño al formulario

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -4,27 +4,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/styles.css">
     <title>Mi primer Crud</title>
 </head>
 <body>
-    <h1>Ingreso de Post</h1>
-    <br>
-    <br>
-    <form action="{{route('post.store')}}" method="post">
-        @csrf 
-        <label for="">Titulo</label>
-        <input type="text" name="Titulo">
+    <header class="centrar">
+        <h1>Ingreso de Post</h1>
+        <br>
+        <br>
+    </header>
+    <main>
+      
+            <form action="{{route('post.store')}}" method="post">
+                @csrf 
+                <div class="container">
+                <section class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <label for="">Titulo</label>
+                        <input type="text" name="Titulo" class="input" placeholder="Ingrese un Titulo">
+                    </div>
+                    
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <label for="">Url Corta</label>
+                        <input type="text" name="slug" class="input" placeholder="Ingrese una URL corta">
+                    </div>
 
-        <label for="">Url Corta</label>
-        <input type="text" name="slug">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <label for="">Contenido</label>
+                        <textarea name="content" class="textarea" placeholder="Ingrese un Contenido"></textarea>
+                    </div>
 
-        <label for="">Contenido</label>
-        <textarea name="content"></textarea>
-
-        <label for="">Descripcion</label>
-        <textarea name="Description"></textarea>
-
-        <button type="submit">Enviar</button>
-    </form>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <label for="">Descripcion</label>
+                        <textarea name="Description" class="textarea" placeholder="Ingrese una Descripcion"></textarea>
+                    </div>
+                    
+                    <div class="col-xs-12 col-sm-12 col-md-12 centrar">
+                        <button type="submit" class="button ">Enviar</button>
+                    </div>
+                </section>
+                </div>  
+            </form>
+        
+      
+    </main>
+    
 </body>
 </html>
